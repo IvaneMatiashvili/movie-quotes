fab32e88ccb8fc1703e0daaca42094a783805558
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-11 01:06:26 +0400

    feat: create a error component for admins page form

--- movie-quotes/resources/views/components/form/error.blade.php
+++ movie-quotes/resources/views/components/form/error.blade.php
@@ -1,5 +1,5 @@
 @props(['name'])
 
 @error($name)
-    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+    <p {{ $attributes->merge(['class' => 'text-red-400 italic absolute text-xs']) }}>{{ $message }}</p>
 @enderror
