5bfbaf0a4ec692e89371e6bbb4a52fdc11e5fc01
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-11 00:54:35 +0400

    feat: create a route to get admin/movie/create and create a post route for admin/movie/create to store form data

---
+++ movie-quotes/resources/views/admin/movies/create.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+        <form method="POST" action="/admin/posts" enctype="multipart/form-data">
+            @csrf
+
+            <x-form.input name="title" required />
+            <x-form.input name="slug" required />
+            <x-form.input name="thumbnail" type="file" required />
+            <x-form.textarea name="excerpt" required />
+            <x-form.textarea name="body" required />
+
+            <x-form.field>
+            </x-form.field>
+
+            <x-form.button>Publish</x-form.button>
+        </form>
+</x-layout>
