b3e4d32e42f52da6cad709a3a637d920a8939908
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-11 13:29:56 +0400

    fix: delete a style element

--- movie-quotes/resources/views/components/layout.blade.php
+++ movie-quotes/resources/views/components/layout.blade.php
@@ -3,11 +3,6 @@
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             @vite('resources/css/app.css')
-            <style>
-                html {
-                    scroll-behavior: smooth;
-                }
-            </style>
 
         <body style="font-family: Open Sans, sans-serif" class="w-full h-screen bg-gray-200">
             <x-header/>
