a046cf0cc8a67a8e00e8d3dd76a763ca02770204
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-11 13:18:37 +0400

    feat: create a route name

--- movie-quotes/routes/web.php
+++ movie-quotes/routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('admin/movies/create', [AdminMovieListController::class, 'create']);
+Route::get('admin/movies/create', [AdminMovieListController::class, 'create'])->name('movies.create');
 Route::post('/admin/movies/create', [AdminMovieListController::class, 'store']);
