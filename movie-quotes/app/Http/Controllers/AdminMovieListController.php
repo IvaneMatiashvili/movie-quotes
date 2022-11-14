f8c16fd34bb459a54ab57f6071b0e8e66e8cba2e
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-14 18:32:01 +0400

    feat: create a full functionality for crud operations for movies

--- movie-quotes/app/Http/Controllers/AdminMovieListController.php
+++ movie-quotes/app/Http/Controllers/AdminMovieListController.php
@@ -5,9 +5,17 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\Movie;
 use App\Models\Quote;
+use Illuminate\Support\Str;
 
 class AdminMovieListController extends Controller
 {
+	public function index()
+	{
+		return view('admin.movies.index', [
+			'movies' => Movie::latest()->paginate(5),
+		]);
+	}
+
 	public function create()
 	{
 		return view('admin.movies.create');
@@ -17,16 +25,37 @@ class AdminMovieListController extends Controller
 	{
 		$movie = Movie::create([
 			'title'     => $request->title,
-			'thumbnail' => $request->file('thumbnail')->store('public/thumbnails'),
+			'slug'      => Str::lower($request->title),
 		]);
 
 		Quote::create([
-			'quote'    => $request->quote,
-			'movie_id' => $movie->id,
+			'quote'     => $request->quote,
+			'movie_id'  => $movie->id,
+			'thumbnail' => $request->file('thumbnail')->store('public/thumbnails'),
 		]);
 
-		$url = route('movies.create');
+		return redirect(route('movies'))->with('success', 'Movie title has created successfully');
+	}
 
-		return redirect($url);
+	public function edit(Movie $movie)
+	{
+		return view('admin.movies.edit', [
+			'movie' => $movie,
+		]);
+	}
+
+	public function update(Movie $movie, StoreUserRequest $request)
+	{
+		$movie->update([
+			'title'     => $request->title,
+			'slug'      => Str::lower($request->title),
+		]);
+		return redirect(route('movies'))->with('success', 'Movie title has updated successfully');
+	}
+
+	public function destroy(Movie $movie)
+	{
+		$movie->delete();
+		return redirect(route('movies'))->with('success', 'Movie title has deleted successfully');
 	}
 }
