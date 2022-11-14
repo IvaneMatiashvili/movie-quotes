6259ba8914dfed6a9c93ee39079deefcdbe7af44
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-14 18:08:37 +0400

    fix:fix a create page markup

--- movie-quotes/resources/views/admin/movies/create.blade.php
+++ movie-quotes/resources/views/admin/movies/create.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-    <x-panel>
-        <form method="POST" action="{{ route('movies.create') }}" enctype="multipart/form-data" class="w-[90%] h-[80%]">
+    <x-panel title="Create content">
+        <form method="POST" action="{{ route('movies') }}" enctype="multipart/form-data" class="w-[90%] h-[80%]">
             @csrf
             <div>
 
@@ -14,7 +14,7 @@
 
             <div class="mt-8">
 
-                <x-form.label name="thumbnail" title="Thumbnail"/>
+                <x-form.label name="thumbnail" title="Quote thumbnail"/>
 
                 <div class="mt-1 h-10 w-64 bg-gray-200 flex items-center">
                     <x-form.input name="thumbnail" type="file"/>
@@ -32,7 +32,13 @@
                 </div>
             </div>
 
-            <x-form.button/>
+            <x-flex.row class="justify-between">
+                <x-form.button title="Submit"/>
+                <div class="flex align-center">
+                    <a href="{{ route('movies') }}" class="hover:overline italic mt-10 text-lg text-gray-500">go back</a>
+                </div>
+
+            </x-flex.row>
         </form>
     </x-panel>
 </x-layout>
