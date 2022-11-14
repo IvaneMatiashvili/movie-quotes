d823e2aacb5114b3ef4dac87d680e0723c3046ce
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-14 18:13:20 +0400

    feat: create a quotes create page markup to create quote for each movie

---
+++ movie-quotes/resources/views/admin/quotes/create.blade.php
@@ -0,0 +1,36 @@
+<x-layout>
+    <x-panel title="Create quote for">
+        <div class="w-full flex flex-col italic justify-center items-center mb-10 ">
+            <P>{{ $movie->title }}</P>
+        </div>
+        <form method="POST" action="{{ route('quotes.create', $movie->slug) }}" enctype="multipart/form-data" class="w-[90%] h-[80%]">
+            @csrf
+            <div class="mt-8">
+
+                <x-form.label name="thumbnail" title="Quote thumbnail"/>
+
+                <div class="mt-1 h-10 w-64 bg-gray-200 flex items-center">
+                    <x-form.input name="thumbnail" type="file"/>
+                    <x-form.error name="thumbnail" class="mt-14"/>
+                </div>
+            </div>
+
+            <div class="mt-8">
+
+                <x-form.label name="quote" title="Quote"/>
+
+                <div class="mt-1">
+                    <x-form.textarea name="quote"/>
+                    <x-form.error name="quote" class="mt-2"/>
+                </div>
+            </div>
+            <x-flex.row class="justify-between">
+                <x-form.button title="Submit"/>
+                <div class="flex align-center">
+                    <a href="{{ route('movies') }}" class="hover:overline italic mt-10 text-lg text-gray-500">go back</a>
+                </div>
+
+            </x-flex.row>
+        </form>
+    </x-panel>
+</x-layout>
