69e10b4d4b098fe278419a2e5375bf3518deab50
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-14 18:34:44 +0400

    feat: create a admin with php artisan command

---
+++ movie-quotes/app/Console/Commands/CreateAdmin.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
+
+class CreateAdmin extends Command
+{
+	protected $signature = 'create:admin';
+
+	protected $description = 'Create admin';
+
+	public function handle()
+	{
+		if (User::count())
+		{
+			$this->info('Admin account already exists');
+		}
+		else
+		{
+			$username = $this->ask('username');
+
+			$password = $this->ask('Password');
+
+			$validator = Validator::make([
+				'username' => $username,
+				'password' => $password,
+			], [
+				'username' => ['required'],
+				'password' => ['required', 'min:4'],
+			]);
+
+			if ($validator->fails())
+			{
+				$this->info('Admin Account not created. See error messages below:');
+
+				foreach ($validator->errors()->all() as $error)
+				{
+					$this->error($error);
+				}
+				return 1;
+				$validator->errors()->all();
+			}
+			else
+			{
+				User::create([
+					'username' => $username,
+					'password' => $password,
+				]);
+
+				$this->info('Admin Account created.');
+			}
+		}
+	}
+}
