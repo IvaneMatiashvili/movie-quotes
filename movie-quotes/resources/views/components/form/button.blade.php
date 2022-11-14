66271ed8b54a6139210165ac6fe9e14c4f04e078
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-14 18:01:05 +0400

    fix: fix buttton markup

--- movie-quotes/resources/views/components/form/button.blade.php
+++ movie-quotes/resources/views/components/form/button.blade.php
@@ -1,5 +1,6 @@
+@props(['title'])
 <button type="submit"
         class="text-slate-500 italic mt-10 bg-gray-300 text-black uppercase font-semibold text-sm py-3 px-10 rounded-lg hover:bg-stone-300"
 >
-    Submit
+    {{ $title }}
 </button>
