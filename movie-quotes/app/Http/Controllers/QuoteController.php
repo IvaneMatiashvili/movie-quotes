42cfe6a907f2bc5b84b0978741c4765d910a650c
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-14 18:32:24 +0400

    feat: create a full functionality for crud operations for quotes

--- movie-quotes/app/Http/Controllers/QuoteController.php
+++ movie-quotes/app/Http/Controllers/QuoteController.php
@@ -2,9 +2,65 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\Movie;
+use App\Models\Quote;
 
 class QuoteController extends Controller
 {
-    //
+	public function index(Movie $movie)
+	{
+		return view('admin.quotes.index', [
+			'quotes' => $movie->quotes()->latest()->paginate(2),
+			'movie'  => $movie,
+		]);
+	}
+
+	public function create(Movie $movie)
+	{
+		return view('admin.quotes.create', [
+			'movie' => $movie,
+		]);
+	}
+
+	public function store(Movie $movie, StoreUserRequest $request)
+	{
+		Quote::create([
+			'quote'     => $request->quote,
+			'movie_id'  => $movie->id,
+			'thumbnail' => $request->file('thumbnail')->store('public/thumbnails'),
+		]);
+
+		return back()->with('success', 'Quote has created successfully');
+	}
+
+	public function edit(Movie $movie, Quote $quote)
+	{
+		return view('admin.quotes.edit', [
+			'movie' => $movie,
+			'quote' => $quote,
+		]);
+	}
+
+	public function update(Movie $movie, Quote $quote, StoreUserRequest $request)
+	{
+		if (isset($request['thumbnail']))
+		{
+			$quote->update([
+				'thumbnail'          => $request->file('thumbnail')->store('thumbnails'),
+			]);
+		}
+
+		$quote->update([
+			'quote'          => $request->quote,
+		]);
+
+		return redirect(route('quotes', $movie->slug))->with('success', 'Quote title has updated successfully');
+	}
+
+	public function destroy(Movie $movie, Quote $quote)
+	{
+		$quote->delete();
+		return redirect(route('quotes', $movie->slug))->with('success', 'Quote has deleted successfully');
+	}
 }
