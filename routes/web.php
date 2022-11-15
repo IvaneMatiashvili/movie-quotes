b480e132ee8bd31419151a6c6cccbaad366c5ea5
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-15 19:48:50 +0400

    feat: creata  login page route and froup relative routes together

--- routes/web.php
+++ routes/web.php
@@ -16,21 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin/login/{username}/{password}', [SessionsController::class, 'store']);
-Route::post('admin/logout', [SessionsController::class, 'destroy'])->name('logout')->middleware('can:admin');
+Route::get('admin/login', [SessionsController::class, 'create'])->name('login')->middleware('guest');
+Route::post('admin/login', [SessionsController::class, 'store'])->middleware('guest');
+Route::post('admin/logout', [SessionsController::class, 'destroy'])->name('logout')->middleware('auth');
 
 //admin movies
-Route::post('/admin/movies', [AdminMovieListController::class, 'store'])->middleware('can:admin');
-Route::get('admin/movies/create', [AdminMovieListController::class, 'create'])->name('movies.create')->middleware('can:admin');
-Route::get('admin/movies', [AdminMovieListController::class, 'index'])->name('movies')->middleware('can:admin');
-Route::get('admin/movies/{movie:slug}/edit', [AdminMovieListController::class, 'edit'])->name('movies.edit')->middleware('can:admin');
-Route::patch('admin/movies/{movie:slug}', [AdminMovieListController::class, 'update'])->name('movies.update')->middleware('can:admin');
-Route::delete('admin/movies/{movie:slug}', [AdminMovieListController::class, 'destroy'])->name('movies.destroy')->middleware('can:admin');
+Route::middleware('auth')->group(function () {
+	Route::post('/admin/movies', [AdminMovieListController::class, 'store']);
+	Route::get('admin/movies/create', [AdminMovieListController::class, 'create'])->name('movies.create');
+	Route::get('admin/movies', [AdminMovieListController::class, 'index'])->name('movies');
+	Route::get('admin/movies/{movie:slug}/edit', [AdminMovieListController::class, 'edit'])->name('movies.edit');
+	Route::patch('admin/movies/{movie:slug}', [AdminMovieListController::class, 'update'])->name('movies.update');
+	Route::delete('admin/movies/{movie:slug}', [AdminMovieListController::class, 'destroy'])->name('movies.destroy');
+});
 
 //admin quotes
-Route::post('/admin/movies/{movie:slug}/quotes/create', [QuoteController::class, 'store'])->middleware('can:admin');
-Route::get('/admin/movies/{movie:slug}/quotes/create', [QuoteController::class, 'create'])->name('quotes.create')->middleware('can:admin');
-Route::get('/admin/movies/{movie:slug}/quotes', [QuoteController::class, 'index'])->name('quotes')->middleware('can:admin');
-Route::get('admin/movies/{movie:slug}/quotes/{quote}/edit', [QuoteController::class, 'edit'])->name('quotes.edit')->middleware('can:admin');
-Route::patch('admin/movies/{movie:slug}/quotes/{quote}', [QuoteController::class, 'update'])->name('quotes.update')->middleware('can:admin');
-Route::delete('admin/movies/{movie:slug}/quotes/{quote}', [QuoteController::class, 'destroy'])->name('quotes.destroy')->middleware('can:admin');
+Route::middleware('auth')->group(function () {
+	Route::post('/admin/movies/{movie:slug}/quotes/create', [QuoteController::class, 'store']);
+	Route::get('/admin/movies/{movie:slug}/quotes/create', [QuoteController::class, 'create'])->name('quotes.create');
+	Route::get('/admin/movies/{movie:slug}/quotes', [QuoteController::class, 'index'])->name('quotes');
+	Route::get('admin/movies/{movie:slug}/quotes/{quote}/edit', [QuoteController::class, 'edit'])->name('quotes.edit');
+	Route::patch('admin/movies/{movie:slug}/quotes/{quote}', [QuoteController::class, 'update'])->name('quotes.update');
+	Route::delete('admin/movies/{movie:slug}/quotes/{quote}', [QuoteController::class, 'destroy'])->name('quotes.destroy');
+});
