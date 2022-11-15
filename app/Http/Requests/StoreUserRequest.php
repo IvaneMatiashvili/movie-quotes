237a709d64498f27238ff7a628c53f4ca3bdeb34
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-15 19:52:31 +0400

    fix: remove uptdate request features

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -14,11 +14,6 @@ class StoreUserRequest extends FormRequest
 	 */
 	public function rules()
 	{
-		return $this->isMethod('POST') ? $this->store() : $this->update();
-	}
-
-	protected function store()
-	{
 		return $this->has('title') ? $this->storeMovie() : $this->storeQuote();
 	}
 
@@ -33,26 +28,6 @@ class StoreUserRequest extends FormRequest
 	{
 		return [
 			'thumbnail' => 'required|image',
-			'quote'     => 'required|max:600',
-		];
-	}
-
-	protected function update()
-	{
-		return $this->has('title') ? $this->updateMovie() : $this->updateQuote();
-	}
-
-	protected function updateMovie()
-	{
-		return [
-			'title'     => ['required', 'max:255', Rule::unique('movies', 'title')->ignore($this->movie['id'])],
-		];
-	}
-
-	protected function updateQuote()
-	{
-		return [
-			'thumbnail' => 'image',
 			'quote'     => 'required|max:600',
 		];
 	}
