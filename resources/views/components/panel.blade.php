759e64c708d5c1f30e6a9fad6bbf335b3afc89b2
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-21 16:01:19 +0400

    fix: fix panel width

--- resources/views/components/panel.blade.php
+++ resources/views/components/panel.blade.php
@@ -1,6 +1,6 @@
 @props(['title'])
 
-<div {{ $attributes(['class' => 'flex flex-col justify-center items-center border-2 border-white p-6 rounded-xl w-1/4 h-[90%]']) }}>
+<div {{ $attributes(['class' => 'flex flex-col justify-center items-center border-2 border-white p-6 rounded-xl w-[35rem] min-h-[60rem]']) }}>
     <p class="italic text-slate-500 text-3xl mb-6">{{ $title }}</p>
     {{ $slot }}
 </div>
