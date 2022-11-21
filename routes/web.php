491e8cd3f0b65be2bcee2a79195df1dceaad4877
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-21 20:26:19 +0400

    feat: create a route for language switcher

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminMovieListController;
 use App\Http\Controllers\HomePageController;
+use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\QuoteController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
@@ -17,35 +18,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//language switcher
+Route::get('lang/{lang}', [LanguageController::class, 'switchLang'])->name('lang.switch');
+
 //home
 Route::middleware('setLocale')->group(function () {
-	Route::get('/{lang}', [HomePageController::class, 'index']);
-	Route::get('/listing/{movie:slug}/{lang}', [HomePageController::class, 'show'])->name('listing');
+	Route::get('/', [HomePageController::class, 'index']);
+	Route::get('/listing/{movie:slug}', [HomePageController::class, 'show'])->name('listing');
 });
 
 //login and logout
 Route::middleware('setLocale')->group(function () {
-	Route::get('admin/login/{lang}', [SessionsController::class, 'create'])->name('login')->middleware('guest');
-	Route::post('admin/login/{lang}', [SessionsController::class, 'store'])->middleware('guest');
-	Route::post('admin/logout/{lang}', [SessionsController::class, 'destroy'])->name('logout')->middleware('auth');
+	Route::get('admin/login', [SessionsController::class, 'create'])->name('login')->middleware('guest');
+	Route::post('admin/login', [SessionsController::class, 'store'])->middleware('guest');
+	Route::post('admin/logout', [SessionsController::class, 'destroy'])->name('logout')->middleware('auth');
 });
 
 //admin movies
 Route::middleware(['auth', 'setLocale'])->group(function () {
-	Route::post('/admin/movies/{lang}', [AdminMovieListController::class, 'store']);
-	Route::get('admin/movies/create/{lang}', [AdminMovieListController::class, 'create'])->name('movies.create');
-	Route::get('admin/movies/{lang}', [AdminMovieListController::class, 'index'])->name('movies');
-	Route::get('admin/movies/{movie:slug}/edit/{lang}', [AdminMovieListController::class, 'edit'])->name('movies.edit');
-	Route::patch('admin/movies/{movie:slug}/{lang}', [AdminMovieListController::class, 'update'])->name('movies.update');
-	Route::delete('admin/movies/{movie:slug}/{lang}', [AdminMovieListController::class, 'destroy'])->name('movies.destroy');
+	Route::post('/admin/movies', [AdminMovieListController::class, 'store']);
+	Route::get('admin/movies/create', [AdminMovieListController::class, 'create'])->name('movies.create');
+	Route::get('admin/movies', [AdminMovieListController::class, 'index'])->name('movies');
+	Route::get('admin/movies/{movie:slug}/edit', [AdminMovieListController::class, 'edit'])->name('movies.edit');
+	Route::patch('admin/movies/{movie:slug}', [AdminMovieListController::class, 'update'])->name('movies.update');
+	Route::delete('admin/movies/{movie:slug}', [AdminMovieListController::class, 'destroy'])->name('movies.destroy');
 });
 
 //admin quotes
 Route::middleware(['auth', 'setLocale'])->group(function () {
-	Route::post('/admin/movies/{movie:slug}/quotes/create/{lang}', [QuoteController::class, 'store']);
-	Route::get('/admin/movies/{movie:slug}/quotes/create/{lang}', [QuoteController::class, 'create'])->name('quotes.create');
-	Route::get('/admin/movies/{movie:slug}/quotes/{lang}', [QuoteController::class, 'index'])->name('quotes');
-	Route::get('admin/movies/{movie:slug}/quotes/{quote}/edit/{lang}', [QuoteController::class, 'edit'])->name('quotes.edit');
-	Route::patch('admin/movies/{movie:slug}/quotes/{quote}/{lang}', [QuoteController::class, 'update'])->name('quotes.update');
-	Route::delete('admin/movies/{movie:slug}/quotes/{quote}/{lang}', [QuoteController::class, 'destroy'])->name('quotes.destroy');
+	Route::post('/admin/movies/{movie:slug}/quotes/create', [QuoteController::class, 'store']);
+	Route::get('/admin/movies/{movie:slug}/quotes/create', [QuoteController::class, 'create'])->name('quotes.create');
+	Route::get('/admin/movies/{movie:slug}/quotes', [QuoteController::class, 'index'])->name('quotes');
+	Route::get('admin/movies/{movie:slug}/quotes/{quote}/edit', [QuoteController::class, 'edit'])->name('quotes.edit');
+	Route::patch('admin/movies/{movie:slug}/quotes/{quote}', [QuoteController::class, 'update'])->name('quotes.update');
+	Route::delete('admin/movies/{movie:slug}/quotes/{quote}', [QuoteController::class, 'destroy'])->name('quotes.destroy');
 });
