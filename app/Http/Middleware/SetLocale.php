340f54bddfd8bd006d83a6d62fa31e8999ee8d45
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-21 20:31:23 +0400

    fix: save languege in session

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -3,13 +3,23 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\App;
 
 class SetLocale
 {
 	public function handle($request, Closure $next)
 	{
-		$lang = $request->route('lang');
-		app()->setLocale($lang);
+		/*		$lang = $request->route('lang');
+				app()->setLocale($lang);*/
+
+		if (Session()->has('applocale') && array_key_exists(Session()->get('applocale'), config('languages')))
+		{
+			App::setLocale(Session()->get('applocale'));
+		}
+		else
+		{
+			App::setLocale(config('app.fallback_locale'));
+		}
 
 		return $next($request);
 	}
