9694462c339249a40e08b57c412deed610d82351
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-21 16:18:10 +0400

    fix: create a request validation for georgian language

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -14,21 +14,47 @@ class StoreUserRequest extends FormRequest
 	 */
 	public function rules()
 	{
-		return $this->has('title') ? $this->storeMovie() : $this->storeQuote();
+		/*		return $this->has('title') ? $this->storeMovie() : $this->storeQuote();*/
+
+		if ($this->has('title') && $this->has('quote'))
+		{
+			return $this->storeMovieAndQuote();
+		}
+		elseif ($this->has('title'))
+		{
+			return $this->storemovie();
+		}
+		else
+		{
+			return $this->storeQuote();
+		}
 	}
 
 	protected function storeMovie()
 	{
 		return [
-			'title'     => ['required', 'max:255', Rule::unique('movies', 'title')],
+			'title'        => ['required', 'max:255', Rule::unique('movies', 'title')],
+			'title-ka'     => ['required', 'max:255', Rule::unique('movies', 'title')],
 		];
 	}
 
 	protected function storeQuote()
 	{
 		return [
-			'thumbnail' => 'required|image',
-			'quote'     => 'required|max:600',
+			'thumbnail'    => 'required|image',
+			'quote'        => 'required|max:600',
+			'quote-ka'     => 'required|max:600',
+		];
+	}
+
+	protected function storeMovieAndQuote()
+	{
+		return [
+			'title'           => ['required', 'max:255', Rule::unique('movies', 'title')],
+			'title-ka'        => ['required', 'max:255', Rule::unique('movies', 'title')],
+			'thumbnail'       => 'required|image',
+			'quote'           => 'required|max:600',
+			'quote-ka'        => 'required|max:600',
 		];
 	}
 }
