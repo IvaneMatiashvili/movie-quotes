6a8bc6364ae1a97b13670a0317c18ddb9e852c0c
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-21 16:01:54 +0400

    fix: config header content for english and geaorgian

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,15 +1,24 @@
-<header class="flex justify-center border-2 border-white p-6 w-full h-[10%] bg-stone-100 bg-gray-200">
+<header class="flex justify-center border-2 border-white p-6 w-full h-[10%] bg-stone-100 bg-gray-200  mb-16">
     <div class="flex justify-between w-full h-full">
-        <x-header.unordered-list class="w-[15%] ml-10">
-            <x-header.list class="mr-10" content="Create" address="{{ route('movies.create') }}"/>
-            <x-header.list content="List of movies" address="{{ route('movies') }}"/>
+        <x-header.unordered-list class="w-[18%] ml-10">
+            <x-header.list class="mr-10" content="{{ __('content.create') }}" address="{{ route('movies.create', request()->segment(count(request()->segments()))) }}"/>
+            <x-header.list content="{{ __('content.list_of_movies') }}" address="{{ route('movies', request()->segment(count(request()->segments()))) }}"/>
         </x-header.unordered-list>
-        <x-header.unordered-list class="w-[8%] mr-10">
-            <form method="post" action="{{ route('logout') }}">
+        <x-header.unordered-list class="w-[15rem] mr-10 justify-between">
+            <form method="post" action="{{ route('logout', request()->segment(count(request()->segments()))) }}">
                 @csrf
 
-                <button type="submit" class="hover:overline">Log out</button>
+                <button type="submit" class="hover:overline">{{ __('content.logout') }}</button>
             </form>
+            <x-header.unordered-list class="w-[5rem] justify-center border-0 border-l-2 rounded-sm">
+                <li class="mr-2 hover:overline">
+                    <a href="{{ 'en' }}" >en</a>
+                </li>
+                <li class="hover:overline">
+
+                    <a href="{{ 'ka' }}">ka</a>
+                </li>
+            </x-header.unordered-list>
         </x-header.unordered-list>
     </div>
 </header>
