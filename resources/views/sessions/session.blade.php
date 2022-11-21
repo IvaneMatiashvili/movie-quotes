bfca68a4003db6febcae5c32f6b2012865aa135a
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-21 15:58:28 +0400

    fix: configure login page conten for english and georgian

--- resources/views/sessions/session.blade.php
+++ resources/views/sessions/session.blade.php
@@ -1,14 +1,14 @@
-<x-index-layout>
-        <x-panel title="Log In">
+<x-session-layout>
+        <x-panel title="{{ __('content.login') }}">
             <div class="w-full flex flex-col italic justify-center items-center mb-10 ">
                 <P></P>
             </div>
-            <form method="POST" action="{{ route('login') }}" enctype="multipart/form-data" class="w-[90%] h-[80%]">
+            <form method="POST" action="{{ route('login', request()->segment(count(request()->segments()))) }}" enctype="multipart/form-data" class="w-[90%] h-[80%]">
                 @csrf
 
                 <div>
 
-                    <x-form.label name="username" title="Username"/>
+                    <x-form.label name="username" title="{{ __('content.username') }}"/>
 
                     <div class="mt-1">
                         <x-form.input name="username" class="h-12" type="text"/>
@@ -17,17 +17,17 @@
                 </div>
                 <div class="mt-10">
 
-                    <x-form.label name="password" title="Password"/>
+                    <x-form.label name="password" title="{{ __('content.password') }}"/>
 
                     <div class="mt-1">
                         <x-form.input name="password" type="password" class="h-12"/>
-                        <x-form.error name="Password" class="mt-2"/>
+                        <x-form.error name="password" class="mt-2"/>
                     </div>
                 </div>
 
                 <x-flex.row class="justify-between">
-                    <x-form.button title="Submit"/>
+                    <x-form.button title="{{ __('content.submit_btn') }}"/>
                 </x-flex.row>
             </form>
         </x-panel>
-</x-index-layout>
+</x-session-layout>
