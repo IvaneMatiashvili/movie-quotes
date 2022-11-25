9014855de7ef374adae22d7e7ce92004f2b62033
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-25 15:50:22 +0400

    fix: add a favicon and change title in layout files

--- resources/views/components/admin-layout.blade.php
+++ resources/views/components/admin-layout.blade.php
@@ -1,8 +1,9 @@
 @auth
 <!doctype html>
-            <title>movie-quotes</title>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Movie Quotes</title>
+            <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
             @vite('resources/css/app.css')
 
         <body style="font-family: Open Sans, sans-serif" class="w-full h-screen bg-gray-200">
