26abda78937a9c1aa70d909b9d7b9d72cdd81ec2
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-11-27 18:26:57 +0400

    fix: fix home page markup

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,14 +1,13 @@
 <x-home-layout current-language="{{ $currentLanguage }}">
-    @if($randomMovie->count())
+    @if($randomMovie->count() && $randomMovie->quotes->count())
 
-        @if($randomMovie->quotes->count())
-            <div class="ml-4 ">
-                <img src="{{ asset('storage/' . $randomQuote->thumbnail)}}"
-                     alt="thumbnail image" class="h-[24.125rem] w-[43.75rem] rounded-xl">
-            </div>
-
-            <p class="mt-14 text-white text-3xl font-sansation">"{{ $randomQuote->quote }}"</p>
-        @endif
+        <div class="ml-4 ">
+            <img src="{{ asset('storage/' . $randomQuote->thumbnail)}}"
+                 alt="thumbnail image" class="h-[24.125rem] w-[43.75rem] rounded-xl">
+        </div>
+        <div class="flex justify-center w-[50rem]">
+            <p class="mt-14 ml-4 text-white flex justify-center items-center text-3xl font-sansation">"{{ $randomQuote->quote }}"</p>
+        </div>
 
         <a href="{{ route('listing', [$randomMovie->slug, request()->segment(count(request()->segments()))]) }}"
            class="underline mt-14 font-sansation text-white text-3xl">"{{ $randomMovie->title }}"</a>
